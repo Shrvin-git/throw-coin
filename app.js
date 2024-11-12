f51d6e1790acca55cf67d58f6f05dc380740b6d1
Shrvin-git <shrvindavoodi@gmail.com>
2024-11-12 08:46:27 +0330

    Update app.js

--- app.js
+++ app.js
@@ -14,38 +14,60 @@ function flipOrcoin(element) {
 
     resultClick = element.innerHTML
     userChoice.innerHTML = resultClick
-
+    userChoice.style.opacity = 1
+    userChoice.style.visibility = 'visible'
 
 }
 
 
 function throwCoin() {
     let randomThrow = Math.floor(Math.random() * coinAndflip.length)
+    message.innerHTML = ''
 
 
-    
     if (resultClick == null || resultClick == undefined) {
-        
         alert('شیر یا خط ؟ یکیشو امتحان کن')
-        
     }
+
+
+
+
     else {
         result.innerHTML = 'سکه داره میچرخه کمی صبر کن'
 
         setTimeout(function () {
-            if (coinAndflip[randomThrow] == resultClick) {
-                message.innerHTML = 'بردی آفرین :))'
-                message.classList.add('result_throw_coin')
-                message.classList.remove('result_throw_coin_lost')
-                result.innerHTML = 'نتیجه را اینجا مشاده کنید'
-            }
-            else {
-                message.innerHTML = 'باختی که :(('
-                message.classList.remove('result_throw_coin')
-                message.classList.add('result_throw_coin_lost')
-                result.innerHTML = 'نتیجه را اینجا مشاده کنید'
 
+            if (resultClick == 'شیر') {
+                if (coinAndflip[randomThrow] == resultClick) {
+                    message.innerHTML = 'آفرین بردی. شیر اومد :)'
+                    message.classList.add('result_throw_coin')
+                    message.classList.remove('result_throw_coin_lost')
+                    result.innerHTML = 'نتیجه را اینجا مشاده کنید'
+                }
+                else {
+                    message.innerHTML = 'خط اومد. باختی :('
+                    message.classList.remove('result_throw_coin')
+                    message.classList.add('result_throw_coin_lost')
+                    result.innerHTML = 'نتیجه را اینجا مشاده کنید'
+                }
             }
+
+
+            if (resultClick == 'خط') {
+                if (coinAndflip[randomThrow] == resultClick) {
+                    message.innerHTML = 'آفرین بردی. خط اومد :)'
+                    message.classList.add('result_throw_coin')
+                    message.classList.remove('result_throw_coin_lost')
+                    result.innerHTML = 'نتیجه را اینجا مشاده کنید'
+                }
+                else {
+                    message.innerHTML = 'شیر اومد. باختی :('
+                    message.classList.remove('result_throw_coin')
+                    message.classList.add('result_throw_coin_lost')
+                    result.innerHTML = 'نتیجه را اینجا مشاده کنید'
+                }
+            }
+
         }, 2200)
     }
 
